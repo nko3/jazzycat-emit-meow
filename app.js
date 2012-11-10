dc23c2c62039bf111c191db6b7090b07a9eb21ad
Todd Wolfson <todd.wolfson@ensighten.com>
2012-11-09 18:30:39 -0800

    Pages defined

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 var express = require('express'),
+    lactate = require('lactate'),
     app = express.createServer();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// Lactate servers for gzipping? (recommendation by Henry)
+app.use('/public', lactate['static'](__dirname + '/dist'));
+app.use('/public', lactate['static'](__dirname + '/public'));
+
+// Pages is the server views dir
+app.set('views', __dirname + '/pages');
+
+// TODO: Deal with layout.html
+app.set('view options', {
+  layout: false
 });
 
-app.listen(8080);
+app.set('view engine', 'jade');
 
+// Send down index.html
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+// Begin listening and log accordingly
+app.listen(8080);
 console.log('Server running at http://localhost:8080/');
