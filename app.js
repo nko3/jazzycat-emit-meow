192802509370c9126135afd62015bca85185505f
Todd Wolfson <todd.wolfson@ensighten.com>
2012-11-09 19:51:03 -0800

    Added Bootstrap URL test for sprites -- confirmed as working =)

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ app.set('view engine', 'jade');
 app.get('/', function (req, res) {
   res.render('index');
 });
+app.get('/bootstrap_url_test', function (req, res) {
+  res.render('bootstrap_url_test');
+});
+app.get('/google_maps', function (req, res) {
+  res.render('google_maps');
+});
 
 // Begin listening and log accordingly
 app.listen(8080);
