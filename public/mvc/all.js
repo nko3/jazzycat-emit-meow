7e57e7995b8ae7c8e63cea87b39a7d3cab4c738b
Jesse Martin <jesse.martin@ensighten.com>
2012-11-09 23:57:44 -0800

    Location hash (start)

--- public/mvc/all.js
+++ public/mvc/all.js
@@ -5,14 +5,15 @@ function MapperViewModel () {
   var self = this;
 
   self.mappers =  ko.observableArray([
-    { mapperName: "Motorcycle Parking Mapper", contributions: 17, lastUpdated: now },
-    { mapperName: "Cat Mapper", contributions: 77, lastUpdated: now },
-    { mapperName: "Beard Mapper", contributions: 21, lastUpdated: now },
-    { mapperName: ":3", contributions: 5, lastUpdated: now }
+    { mapperName: "Motorcycle Parking Mapper", contributions: 17, lastUpdated: now, folder: 'motorcycle' },
+    { mapperName: "Cat Mapper", contributions: 77, lastUpdated: now, folder: 'cat' },
+    { mapperName: "Beard Mapper", contributions: 21, lastUpdated: now, folder: 'beard' },
+    { mapperName: ":3", contributions: 5, lastUpdated: now, folder: 'kitteh' }
   ]);
 
   self.selectMapper = function (mapper) {
 console.log('selected', mapper);
+    location.hash = mapper.folder;
   };
 }
 
