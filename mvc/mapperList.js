90b948046a5417e59fcfa4dc6eaeb9cb4ae79773
Henry Allen-Tilford <htilford@gmail.com>
2012-11-10 06:53:20 -0800

    cleanup

--- mvc/mapperList.js
+++ mvc/mapperList.js
@@ -1,18 +1,48 @@
 var mapperViewModel,
   now = new Date();
 
+
+
+function Mapper (data, parent) {
+  console.log(parent);
+  var self = this;
+  self.mapperName = data.name;
+  self.contributions = data.contributions;
+  self.lastUpdated = new Date();
+  self.folder = data.folder;
+  self.select = parent.selectMapper;
+}
+
 function MapperViewModel () {
   var self = this;
 
   self.mappers =  ko.observableArray([
-    { mapperName: "Motorcycle Parking Mapper", contributions: 17, lastUpdated: now, folder: '/motorcycle' },
-    { mapperName: "Cat Mapper", contributions: 77, lastUpdated: now, folder: '/cat' },
-    { mapperName: "Beard Mapper", contributions: 21, lastUpdated: now, folder: '/beard' },
-    { mapperName: ":3", contributions: 5, lastUpdated: now, folder: '/kitteh' }
-  ]);
+    {
+      name: "Motorcycle Parking Mapper",
+      contributions: 17,
+      folder: '/motorcycle'
+    },
+    {
+      name: "Cat Mapper",
+      contributions: 77,
+      folder: '/cat'
+    },
+    {
+      name: "Beard Mapper",
+      contributions: 21,
+      folder: '/beard'
+    },
+    {
+      name: ":3",
+      contributions: 5,
+      folder: '/kitteh'
+    }
+  ].map(function (data) {
+    return new Mapper(data, self);
+  }));
 
   self.selectMapper = function (mapper) {
-console.log('selected', mapper);
+    console.log('selected', mapper);
     location.hash = mapper.folder;
 
     // Load selected mapper
