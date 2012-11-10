3fdbde083fe39aacc2c5e72afee83cd7dd34bcdb
Jesse Martin <jesse.martin@ensighten.com>
2012-11-09 20:17:38 -0800

    Knockout like a boss

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,13 @@
+function AppViewModel () {
+  // Non-editable catalog data - would come from the server
+  self.mappers = [
+    { mapperName: "Standard (sandwich)", contributions: 2, lastUpdated: 2 },
+    { mapperName: "Standard (sandwich)", contributions: 3, lastUpdated: 0 },
+    { mapperName: "Standard (sandwich)", contributions: 2, lastUpdated: 1 },
+    { mapperName: "Standard (sandwich)", contributions: 5, lastUpdated: 0 }
+  ];
+}
+
 $(function() {
-  function AppViewModel () {
-
-  }
-
   ko.applyBindings(new AppViewModel());
 });
