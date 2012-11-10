9bd82bf865ddcdda10b2de90b209ca0384c4ed55
Henry Allen-Tilford <htilford@gmail.com>
2012-11-10 13:23:31 -0800

    sweet

--- grunt.js
+++ grunt.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
       'public': {
         src: [
           // sugar
-          'public/js/sugar.js',
+          'public/sugar.js',
           // jQuery, then Bootstrap
           'public/js/jquery.js', 'public/js/bootstrap.js',
           // then Knockout
