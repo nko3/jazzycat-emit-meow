3bfd0cd88af35d8c6f79de49d3660ac1229ff550
Henry Allen-Tilford <htilford@gmail.com>
2012-11-09 17:09:02 -0800

    NAME

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello Mapper-maker\n');
 }).listen(8000);
 
 console.log('Server running at http://0.0.0.0:8000/');
