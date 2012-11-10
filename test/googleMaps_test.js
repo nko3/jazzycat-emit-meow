86320b45a2810444658b21023282abea7a2a5066
Henry Allen-Tilford <htilford@gmail.com>
2012-11-09 23:09:51 -0800

    test

--- test/googleMaps_test.js
+++ test/googleMaps_test.js
@@ -1,5 +1,8 @@
-/*jshint expr:true*/
+/*jshint expr:true, evil: true*/
 /*globals google*/
+
+document.write('<script src="//maps.google.com/maps/api/js?sensor=true"></script>');
+
 describe('google maps', function () {
   it('should exist', function () {
     google.should.exist;
