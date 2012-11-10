616bb391d9d291a8ed8066295666d12f9cb423b3
Henry Allen-Tilford <htilford@gmail.com>
2012-11-09 19:59:37 -0800

    tests

---
+++ test/bootstrapper_test.js
@@ -0,0 +1,7 @@
+/*jshint expr:true*/
+/*globals $*/
+describe('bootstrapper', function () {
+  it('should exist', function () {
+    $().alert.should.exist;
+  });
+});
---
+++ test/knockout_test.js
@@ -0,0 +1,7 @@
+/*jshint expr:true*/
+/*globals ko*/
+describe('knockout', function () {
+  it('should exist', function () {
+    ko.should.exist;
+  });
+});
