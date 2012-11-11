1715bf5d2139bf1033fd8a4d8475761f7af382ed
Jesse Martin <jesse.martin@ensighten.com>
2012-11-11 15:10:59 -0800

    Extracted contributor from meta and changed in /:mapper view

--- models/contribution.js
+++ models/contribution.js
@@ -1,8 +1,10 @@
 function Contribution (data, parent) {
-  var self = this;
+  var self = this,
+    meta = data.meta || {};
+
   self.lat = data.lat;
   self.lng = data.lng;
   self.keywords = ko.observableArray(data.keywords);
-  self.meta = ko.observable(data.meta || {});
-  self.metaStr = JSON.stringify(data.meta);
+  self.meta = ko.observable(meta);
+  self.contributor = meta.contributor;
 }
