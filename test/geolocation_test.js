93f171885f8bf5388941fa46499cbbe1c4a6b5e7
Henry Allen-Tilford <htilford@gmail.com>
2012-11-10 21:02:27 -0800

    TESTS

--- test/geolocation_test.js
+++ test/geolocation_test.js
@@ -3,4 +3,8 @@ describe('geolocation', function () {
   it('should exist', function () {
     window.getCoordinates.should.exist;
   });
+
+  it('should get coordinates', function (done) {
+    getCoordinates(done);
+  });
 });
