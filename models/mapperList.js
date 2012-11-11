24370e2af1448ce19cf8f26c65d8dcdf78e9c5ed
Todd Wolfson <todd.wolfson@ensighten.com>
2012-11-11 12:10:37 -0800

    Updated to use API again

--- models/mapperList.js
+++ models/mapperList.js
@@ -2,7 +2,7 @@ var mapperViewModel;
 
 function Mapper (data, parent) {
   var self = this;
-  self.mapperName = data.keyword;
+  self.mapperName = data.name;
   self.contribution_ids = data.contribution_ids;
   self.numContributions = data.count || 0;
   self.lastUpdated = new Date();
@@ -19,22 +19,23 @@ function Mapper (data, parent) {
 
 function MapperViewModel () {
   var self = this;
-  // $.getJSON('http://jazzycat-emit-meow-api.nko3.jit.su/keyword',
-    // function (data, status) {
-    // if(status === 'success') {
-      // console.log(data.length);
-      // data.forEach(function (mapper) {
-        // self.mappers.push(new Mapper(mapper, self));
-      // });
-    // }
-  // });
-  setTimeout(function () {
-    var data = [{'keyword': 'you', 'count': 20}, {'keyword': 'are', 'count': 10}, {'keyword': 'on', 'count': 3}, {'keyword': 'dev', 'count': 19}];
-    console.log(data.length);
-    data.forEach(function (mapper) {
-      self.mappers.push(new Mapper(mapper, self));
-    });
-  }, 100);
+  $.getJSON('http://jazzycat-emit-meow-api.nko3.jit.su/keyword',
+    function (data, status) {
+    if(status === 'success') {
+      console.log(data.length);
+      data.forEach(function (mapper) {
+        self.mappers.push(new Mapper(mapper, self));
+      });
+    }
+  });
+  // // DEV FOR ITEMS
+  // setTimeout(function () {
+    // var data = [{'keyword': 'you', 'count': 20}, {'keyword': 'are', 'count': 10}, {'keyword': 'on', 'count': 3}, {'keyword': 'dev', 'count': 19}];
+    // console.log(data.length);
+    // data.forEach(function (mapper) {
+      // self.mappers.push(new Mapper(mapper, self));
+    // });
+  // }, 100);
 
   self.mappers = ko.observableArray();
 
