60bddd3e6dc6a6a25382948e126e36abc0b4613d
Todd Wolfson <todd.wolfson@ensighten.com>
2012-11-11 13:14:08 -0800

    Map loading woot

--- dist/models/all.js
+++ dist/models/all.js
@@ -71,7 +71,56 @@ $(function() {
 
 
 function loadMapperMap() {
-  $('#xap').text('zzzzzzzz');
+  var mapper = mapperViewModel.selectedMapper(),
+      contributions = mapper.contributions();
+  console.log(contributions);
+
+  // Create list of cities
+  var SF = {'lat': 37.7750, 'lng': -122.4183},
+      SAN_JOSE = {'lat': 37.3041, 'lng': -121.8727},
+      NYC = {'lat': 40.7142, 'lng': -74.0064},
+      LONDON = {'lat': 51.5171, 'lng': -0.1062},
+      MOSCOW = {'lat': 55.7517, 'lng': 37.6178},
+      ZANZIBAR = {'lat': -6.1333, 'lng': 39.3167},
+      SHANGHAI = {'lat': 31.2000, 'lng': 121.5000};
+
+  // Populate the list of locations with our cities (stub data)
+  var locations = [
+    SF,SF,SF,SF,SF,SF,SF,SF,SF,SF,SF,SF,SF,SF,
+    SAN_JOSE,SAN_JOSE,SAN_JOSE,SAN_JOSE,SAN_JOSE,SAN_JOSE,SAN_JOSE,SAN_JOSE,
+    NYC,NYC,NYC,NYC,
+    LONDON,
+    MOSCOW,MOSCOW,MOSCOW,MOSCOW,MOSCOW,MOSCOW,
+    ZANZIBAR,ZANZIBAR,ZANZIBAR,ZANZIBAR,ZANZIBAR,ZANZIBAR,ZANZIBAR,
+    SHANGHAI
+  ];
+
+  // Generate our map
+  var map = new GMaps({
+    div: '#map',
+    lat: SF.lat,
+    lng: SF.lng
+  });
+
+  // DEV: Break all pass-by-objects to avoid GMaps deletion of cities
+  locations = JSON.parse(JSON.stringify(locations));
+
+  // Render locations to markers
+  var markers = locations.map(function (location) {
+    var marker = map.createMarker(location);
+    return marker;
+  });
+
+  // Add the markers to the map manually (a small hack for MarkerClusterer)
+  map.markers.push.apply(map.markers, markers);
+
+  // Create a marker clusterer on the locations
+  var mcOptions = {gridSize: 50, maxZoom: 15},
+      mc = new MarkerClusterer(map.map, markers, mcOptions);
+
+  // Fit the zoom to the markers on the map
+  map.fitZoom();
+
 }
 /*global mapperViewModel*/
 
